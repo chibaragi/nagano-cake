127667d2af0e27c55084718024a4712865638379
飯田恭子 <iidakyouko@mba.local>
2020-06-27 16:08:42 +0900

    修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -15,7 +15,7 @@ class Product < ApplicationRecord
   # 値段設定時、半角数字のみ登録可能にするバリデーション
   validates :price, presence: true, format: {
     with: /\A[0-9]+\z/,
-    message: "価格は半角数字で入力してください",
+    message: "価格は半角数字で入力してください"
   }
 
   # 検索機能（部分検索）
